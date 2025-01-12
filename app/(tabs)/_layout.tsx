13947f010b412f536ca1813250aa553e83935941
Aydin <thefickers@outlook.com>
2025-01-13 10:21:32 +1100

    adjusted tab bar to be orange when we select the tabs.
    The background highlight is playing up. Need to wrap text + icon in a view

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,20 +1,23 @@
 import { Tabs } from "expo-router";
 import React from "react";
-import { Platform } from "react-native";
+import { Platform, StyleSheet, View } from "react-native";
 
 import { HapticTab } from "@/components/HapticTab";
 import { IconSymbol } from "@/components/ui/IconSymbol";
 import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme ?? "light"];
 
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: theme.yellow,
+        tabBarInactiveTintColor: theme.icon,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
@@ -25,6 +28,22 @@ export default function TabLayout() {
           },
           default: {},
         }),
+        tabBarLabel: ({ focused, children, color }) => (
+          <ThemedText
+            style={[
+              styles.label,
+              {
+                color,
+                transform: [{ scale: focused ? 1.1 : 1 }],
+              },
+            ]}
+          >
+            {children}
+          </ThemedText>
+        ),
+        tabBarIconStyle: {
+          marginTop: 5,
+        },
       }}
       initialRouteName="Home"
     >
@@ -39,8 +58,23 @@ export default function TabLayout() {
         name="Home"
         options={{
           title: "Home",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <View
+              style={
+                focused
+                  ? [
+                      styles.activeIconContainer,
+                      { backgroundColor: theme.yellow + "20" },
+                    ]
+                  : null
+              }
+            >
+              <IconSymbol
+                size={focused ? 32 : 28}
+                name="house.fill"
+                color={color}
+              />
+            </View>
           ),
         }}
       />
@@ -48,8 +82,23 @@ export default function TabLayout() {
         name="Calendar"
         options={{
           title: "Calendar",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="calendar" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <View
+              style={
+                focused
+                  ? [
+                      styles.activeIconContainer,
+                      { backgroundColor: theme.yellow + "20" },
+                    ]
+                  : null
+              }
+            >
+              <IconSymbol
+                size={focused ? 32 : 28}
+                name="calendar"
+                color={color}
+              />
+            </View>
           ),
         }}
       />
@@ -57,11 +106,40 @@ export default function TabLayout() {
         name="Profile"
         options={{
           title: "Profile",
-          tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="person.fill" color={color} />
+          tabBarIcon: ({ color, focused }) => (
+            <View
+              style={
+                focused
+                  ? [
+                      styles.activeIconContainer,
+                      { backgroundColor: theme.yellow + "20" },
+                    ]
+                  : null
+              }
+            >
+              <IconSymbol
+                size={focused ? 32 : 28}
+                name="person.fill"
+                color={color}
+              />
+            </View>
           ),
         }}
       />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  label: {
+    fontSize: 12,
+    fontWeight: "500",
+    marginBottom: 5,
+  },
+  activeIconContainer: {
+    padding: 8,
+    borderRadius: 50,
+    paddingHorizontal: 45,
+    // paddingVertical: 10,
+  },
+});
